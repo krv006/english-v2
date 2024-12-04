2d03bd3425500254ac321ee84a8201836e593ea7
krv006 <rvkamronbek@gmail.com>
2024-12-04 19:36:13 +0500

    model

--- apps/models.py
+++ apps/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractUser
-from django.db.models import EmailField, BooleanField, Model, CharField, DateTimeField
+from django.db.models import EmailField, BooleanField, Model, CharField, DateTimeField, ImageField, SlugField
 from django.db.models import TextChoices, Model
+from django.utils.text import slugify
 
 from apps.managers import CustomUserManager
 
@@ -24,3 +25,24 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
+
+
+class Books(Model):
+    name = CharField(max_length=255)
+    image = ImageField(upload_to='book_image/', blank=True, null=True)
+    # size = PositiveIntegerField(default=0, blank=True, null=True)
+    created_at = DateTimeField(auto_now_add=True)
+    slug = SlugField(max_length=255, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            original_slug = self.slug
+            counter = 1
+            while self.__class__.objects.filter(slug=self.slug).exists():
+                self.slug = f"{original_slug}-{counter}"
+                counter += 1
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
